d25c73cd66565d6552766196a20b8cf51114746c
Crowdbotics <team@crowdbotics.com>
2020-10-21 13:37:50 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps153374', human_name: 'Maps', access_route: 'Maps153374', icon: 'map'},
 { name: 'ArticleList153353', human_name: 'Article List', access_route: 'ArticleList153353'},
 { name: 'ArticleList153352', human_name: 'Article List', access_route: 'ArticleList153352'},
 { name: 'ArticleList153351', human_name: 'Article List', access_route: 'ArticleList153351'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps153374Navigator from '../features/Maps153374/navigator';
 import ArticleList153353Navigator from '../features/ArticleList153353/navigator';
 import ArticleList153352Navigator from '../features/ArticleList153352/navigator';
 import ArticleList153351Navigator from '../features/ArticleList153351/navigator';
@@ -16,6 +17,7 @@ import ArticleList153351Navigator from '../features/ArticleList153351/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Maps153374: { screen: Maps153374Navigator },
 ArticleList153353: { screen: ArticleList153353Navigator },
 ArticleList153352: { screen: ArticleList153352Navigator },
 ArticleList153351: { screen: ArticleList153351Navigator },
